985385ad89220daedd705239684348bfe0b37983
Mju6004101362 <68525406+Mju6004101362@users.noreply.github.com>
2020-11-02 16:52:58 +0700

    movie2

--- components/topbar.vue
+++ components/topbar.vue
@@ -74,7 +74,7 @@ export default {
   align-items: center;
   width: 100%;
   height: 60px;
-  background-color: #000000;
+  background-color: #ffffff;
   -webkit-box-shadow: 0px 4px 5px 0px rgba(0, 0, 0, 0.2);
   -moz-box-shadow: 0px 4px 5px 0px rgba(0, 0, 0, 0.2);
   box-shadow: 0px 4px 5px 0px rgba(0, 0, 0, 0.2);
